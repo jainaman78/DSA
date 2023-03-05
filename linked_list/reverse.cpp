b01cddc282b1f3eb94618946277d15ac96569706
jainaman78 <jainamn9003@gmail.com>
2023-03-05 16:37:13 +0530

    reverse

---
+++ linked_list/reverse.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//node class
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node (int data){
+        this->data=data;
+        next=NULL;
+    }
+};
+
+//print
+void print(node* head){
+    node*temp=head;
+    cout<<head->data<<endl;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+//insertion input
+
+node* takeinput(){
+    int data;
+    cin>>data;
+    node *head=NULL;
+    while(data!=-1){
+        node* newnode=new node(data);
+        if(head==NULL){
+            head=newnode;
+        }
+        else{
+            node* temp=head;
+            while(temp->next!=NULL){
+                temp=temp->next;
+            }
+            temp->next=newnode;
+        }
+        cin>>data;
+
+    }
+    return head;
+}
+// reverse
+
+node* reverse(node* head){
+    node* cur=head;
+    node* pre=NULL;
+
+    while(cur!=NULL){
+        node* temp=cur->next;
+        cur->next=pre;
+        pre=cur;
+        cur=temp;
+    }
+    return pre;
+}
+
+
+int main(){
+    node* head=takeinput();
+    print(head);
+
+}
