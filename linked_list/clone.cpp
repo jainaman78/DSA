dedcbce3958c201619ff35630efacd4405c0c9f3
jainaman78 <jainaman9003@gmail.com>
2023-04-15 22:44:31 +0530

    merge

--- linked_list/clone.cpp
+++ linked_list/clone.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
   class Solution {
 public:
     Node* copyRandomList(Node* head) {
