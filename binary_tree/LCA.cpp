bb650fd3912fa5dad32aab3936e3d69ce4a917dc
jainaman78 <jainaman9003@gmail.com>
2023-04-23 13:42:03 +0530

    LCA

---
+++ binary_tree/LCA.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==p->val || root->val==q->val){
+            return root;
+        }
+        TreeNode* left=lowestCommonAncestor( root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+        if(left!=NULL && right==NULL){
+            return left;
+        }
+        if(left==NULL && right!=NULL ){
+            return right;
+        }
+        if(left!=NULL && right!=NULL){
+            return root;
+        }
+        else{
+            return NULL;
+        }
+
+    }
+};
